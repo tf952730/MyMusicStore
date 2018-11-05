d9cd9f4946f0d470c7ff02fad01b409be8fa5963
tf952730 <1349978569@qq.com>
2018-11-05 18:21:10 +0800

    EF优先作业

---
+++ Demo/CodeFirstDemo/CodeFirstDemo/CodeFirstModels/Department.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstDemo.CodeFirstModels
+{
+   public  class Department
+    {
+        //实体的主键，属性的命名规则：ID、Id、类名+ID、类名+Id
+        public Guid ID { get; set; }
+        //学院名称
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string SortCode { get; set; }
+
+        public Department()
+        {
+            ID = Guid.NewGuid();
+        }
+    }
+}
---
+++ Demo/CodeFirstDemo/CodeFirstDemo/Migrations/StudentSeed.cs
@@ -0,0 +1,66 @@
+﻿using CodeFirstDemo.CodeFirstModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstDemo.Migrations
+{
+   public class StudentSeed
+    {
+        public static void Seed(CourseContext context) {
+            var stu1 = new Student()
+            {
+                StudentCode = "0001",
+                Name = "张三",
+                Address = "崆峒",
+                Birtday = DateTime.Parse("2000-01-01"),
+                Phone = "123456789124",
+                Department = context.Departments.SingleOrDefault(x => x.Name == "电子信息工程学院")
+            };
+            var stu2 = new Student()
+            {
+                StudentCode = "0002",
+                Name = "李四",
+                Address = "峨眉",
+                Birtday = DateTime.Parse("2000-01-01"),
+                Phone = "123456789124",
+                Department = context.Departments.SingleOrDefault(x => x.Name == "电子信息工程学院")
+            };
+            var stu3 = new Student()
+            {
+                StudentCode = "0003",
+                Name = "王五",
+                Address = "少林寺",
+                Birtday = DateTime.Parse("2000-01-01"),
+                Phone = "123456789124",
+                Department = context.Departments.SingleOrDefault(x => x.Name == "电子信息工程学院")
+            };
+            var stu4 = new Student()
+            {
+                StudentCode = "0004",
+                Name = "赵六",
+                Address = "武当",
+                Birtday = DateTime.Parse("2000-01-01"),
+                Phone = "123456789124",
+                Department = context.Departments.SingleOrDefault(x => x.Name == "电子信息工程学院")
+            };
+            var stu5 = new Student()
+            {
+                StudentCode = "0005",
+                Name = "田七",
+                Address = "昆仑山",
+                Birtday = DateTime.Parse("2000-01-01"),
+                Phone = "123456789124",
+                Department = context.Departments.SingleOrDefault(x => x.Name == "电子信息工程学院")
+            };
+            context.Students.Add(stu1);
+            context.Students.Add(stu2);
+            context.Students.Add(stu3);
+            context.Students.Add(stu4);
+            context.Students.Add(stu5);
+            context.SaveChanges();
+        }
+    }
+}
