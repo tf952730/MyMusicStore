dae4d515f39f85ced9e6a935f8609f8df3713f0e
tf952730 <1349978569@qq.com>
2018-11-11 18:09:25 +0800

    11-11

---
+++ WebFormDemo/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebFormDemo.Startup))]
+namespace WebFormDemo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ WebFormDemo/Default.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+  你真好，你真是个大傻吊！！！
+</asp:Content>
