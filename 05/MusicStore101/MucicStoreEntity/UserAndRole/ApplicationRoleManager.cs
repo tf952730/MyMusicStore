0e4e5566cfaffe01689af1ee2c81e81385a0880f
tf952730 <1349978569@qq.com>
2018-11-26 18:24:19 +0800

    11-26

---
+++ 05/MusicStore101/MucicStoreEntity/UserAndRole/ApplicationRoleManager.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Identity;
+
+namespace MucicStoreEntity.UserAndRole
+{
+    public class ApplicationRoleManager : RoleManager<ApplicationRole>
+    {
+        public ApplicationRoleManager(IRoleStore<ApplicationRole, string> store)
+            : base(store)
+        {
+        }
+    }
+}
---
+++ 05/MusicStore101/MusicStore/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using MucicStoreEntity.UserAndRole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        
+       //public string TestLogin(string username="messi",string pwd="123.abc")
+       // {
+       //     var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new MucicStoreEntity.MusicContext()));
+       // }
+    }
+}
